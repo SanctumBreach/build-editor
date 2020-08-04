4f65fb272be2dff96ef2e88937a1fdff9a74b8cc
AlphaToTaC <69196920+AlphaToTaC@users.noreply.github.com>
2020-08-04 16:25:39 +0200

    Created the save/load feature
    wow...I that was fast :) no its not good. no this will prob not hold up too long. but man this is a easy cheat :)

--- js/editor.js
+++ js/editor.js
@@ -1,4 +1,6 @@
 //no don't look here its ugly why would you do this to yourself T_T
+var version="A1.9";
+
 function init(){
 	$("#class_selector").change(function(){
 		console.log($(this).val()+":was selected");
@@ -19,6 +21,102 @@ function init(){
 	PopulatePassiveTrees();
 	resetAllTrees();
 }
+
+function save(){	
+	prompt("Please Copy everything below", _save());
+}
+function _save(){
+	var saveFile="";
+	saveFile+="version:"+version+";";
+	saveFile+="#class_selector:"+$("#class_selector")[0].selectedIndex+";";
+	saveFile+="#Mastery_selector:"+$("#Mastery_selector")[0].selectedIndex+";";
+	saveFile+="BlueBorder:"+saveIds($(".borderBlue"));
+	saveFile+="GreenBorder:"+saveIds($(".borderGreen"));
+	return saveFile;
+}
+
+function saveIds(arr){
+	var txt="";
+	for(var i=0;i<arr.length;i++){		
+		txt+=arr[i].id;
+		if(i<arr.length-1)
+			txt+=",";
+		else
+			txt+=";";
+	}
+	return txt;
+}
+
+function load(){
+	var backup=_save();	
+	var loadfile=prompt("Please Paste the build below", "Harry Potter");
+	_load(loadfile,backup);
+	calculateBonuses();
+}
+function _load(saveFile,backup){
+	if(saveFile==="_reset")
+		return;
+	try{
+		var values=saveFile.split(';');
+		for(var i=0;i<values.length;i++){
+			var keyValuePair=values[i].split(':');
+			switch(keyValuePair[0]){
+				case "version":{
+					console.log(version+":"+keyValuePair[1])
+					if(version!==keyValuePair[1]){
+						alert("you are using a outdated tree. No I haven't made backwards capability yet.");
+						return _load(backup,"_reset");
+					}					
+					break;
+				}
+				case "#class_selector":{
+					console.log(keyValuePair[1]);
+					$("#class_selector option").eq(parseInt(keyValuePair[1])).prop('selected',true).change();
+					break;
+				}
+				case "#Mastery_selector":{
+					console.log(keyValuePair[1]);
+					$("#Mastery_selector option").eq(parseInt(keyValuePair[1])).prop('selected',true).change();
+					break;
+				}
+				case "BlueBorder":{
+					console.log(keyValuePair[1]);
+					if(keyValuePair[1]!=="")
+						_AddClass(keyValuePair[1].split(","),"borderBlue");
+					break;
+				}
+				case "GreenBorder":{
+					console.log(keyValuePair[1]);
+					if(keyValuePair[1]!==""){
+						_RemoveClass(keyValuePair[1].split(","),"borderBlue");
+						_AddClass(keyValuePair[1].split(","),"borderGreen");
+					}
+					break;
+				}			
+				default:
+					break;
+			}
+		}
+	}
+	catch(Exception){
+		alert("could not parse file: "+Exception);
+		return _load(backup,"_reset");
+	}
+	
+}
+function _AddClass(arr,className){
+	for(var i=0;i<arr.length;i++){
+		console.log("id:"+arr[i]+" index:"+i);
+		$("#"+arr[i]).addClass(className);
+	}
+}
+function _RemoveClass(arr,className){
+	for(var i=0;i<arr.length;i++){
+		console.log("id:"+arr[i]+" index:"+i);
+		$("#"+arr[i]).removeClass(className);
+	}
+}
+
 function loadSubclass(PlayerClass){
 	var arr=classes[PlayerClass];
 	var addOptions="";
