1b0cbf6d8e4cd7740db8cfcbbcfd30aa2b5e29bb
AlphaToTaC <69196920+AlphaToTaC@users.noreply.github.com>
2020-08-08 19:49:08 +0200

    Create Skills.js

---
+++ js/data/Skills.js
@@ -0,0 +1,21 @@
+var PassiveTrees=[
+	{	"Name":"Elementalist",
+		"Skills":[
+			{"name":"Lightning Strike",	"type":"Ranged","sclaesOf":"Spell",	"min-base-damage":6,	"max-base-damage":7,	"cast-time":1.4,	"Targets":3},
+			{"name":"Breath Of Fire",	"type":"Melee","sclaesOf":"Weapon",	"min-base-damage":90,	"max-base-damage":110,	"cast-time":1,		"AOEType":"Cone","Angle":60,"mana-cost":10},
+		]
+	},
+	{	"Name":"Spellblade",
+		"Skills":[
+			{"name":"Lightning Strike",	"type":"Ranged","sclaesOf":"Spell",	"min-base-damage":6,	"max-base-damage":7,	"cast-time":1.4,	"Targets":3},
+			{"name":"Breath Of Fire",	"type":"Melee","sclaesOf":"Weapon",	"min-base-damage":90,	"max-base-damage":110,	"cast-time":1,		"AOEType":"Cone","Angle":60,"mana-cost":10},
+		]
+	},
+	{	"Name":"Hexlord",
+		"Skills":[
+			{"name":"Lightning Strike",	"type":"Ranged","sclaesOf":"Spell",	"min-base-damage":6,	"max-base-damage":7,	"cast-time":1.4,	"Targets":3},
+			{"name":"Breath Of Fire",	"type":"Melee","sclaesOf":"Weapon",	"min-base-damage":90,	"max-base-damage":110,	"cast-time":1,		"AOEType":"Cone","Angle":60,"mana-cost":10},
+		]
+	},
+	
+]
